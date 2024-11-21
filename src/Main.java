4bc550e61fa19fe54d734544435c90c0524a12fe
Esben <esbenviinblad@gmail.com>
2024-11-21 12:53:10 +0100

    adding makeSound implementation in Dog and Cat

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Animal dog = new Dog("Buddy");
+        Animal cat = new Cat("Whiskers");
+
+        System.out.println(dog.getName() + " says:");
+        dog.makeSound();
+
+        System.out.println(cat.getName() + " says:");
+        cat.makeSound();
     }
 }
