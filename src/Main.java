1ad277b63f94f87b2d768e70b873e02932b7a3e6
Esben <esbenviinblad@gmail.com>
2024-11-21 13:13:36 +0100

    Added Main.java with runnable main code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Animal dog = new Dog("Buddy");
+        Animal dog = new Dog("Boogie");
         Animal cat = new Cat("Whiskers");
 
         System.out.println(dog.getName() + " says:");
