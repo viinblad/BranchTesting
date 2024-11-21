84ed0feb15a99d2431e55b9b5e008d7eb0897c62
Esben <esbenviinblad@gmail.com>
2024-11-21 12:48:57 +0100

    adding Animal.java

---
+++ src/Animal.java
@@ -0,0 +1,14 @@
+public abstract class Animal {
+    protected String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // Abstract metode, der implementeres i subclasses
+    public abstract void makeSound();
+}
