2a08b9a1a243b48f6e04557a056280d059651fa3
Esben <esbenviinblad@gmail.com>
2024-11-22 08:34:58 +0100

    add test in main to run main code

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,14 @@ public class Main {
         dog.eat("bone");
         dog.showMood(true);  // Test for happy dog
         dog.showMood(false); // Test for unhappy dog
+        dog.jump(3);
+
 
         System.out.println(cat.getName() + " says:");
         cat.makeSound();
         cat.eat("fish");
         cat.showMood(true);  // Test for happy cat
         cat.showMood(false); // Test for unhappy cat
+        cat.jump(2);
     }
 }
